c4d74f36cdefa762afa2a64985cc58abd7c506da
phuhuykoxl <phuhuy02003@gmail.com>
2024-10-18 15:35:54 +0700

    add customs command for access add new goods screen

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,22 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginAndNavigateToAddProduct', () => {
+    // Truy cập trang đăng nhập
+    cy.visit('https://hvtester.pos365.vn/Signin');
+    
+    cy.get('input.login-user').type('0916591010');
+    cy.get('input.login-pw').type('vienngocden92');
+    cy.get('button.btn-login').click();
+    cy.wait(500);
+    
+    // Kiểm tra đăng nhập thành công và điều hướng đến trang thêm hàng hóa
+    cy.url().should('include', '/Dashboard');
+    cy.get('a').contains('Hàng hóa').click();
+    cy.get('a').contains('Danh sách hàng hóa').click();
+    cy.url().should('include', '/Products');
+    cy.wait(500);
+    cy.get('button').contains('Thêm mới Hàng hóa').click();
+    cy.wait(500);
+  });
+  
